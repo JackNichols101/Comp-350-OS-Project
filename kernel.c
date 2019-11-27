0e3aa379c0c706b63d27065690018d49d7c13c88
JackNichols101 <57160810+JackNichols101@users.noreply.github.com>
2019-11-26 23:31:37 -0500

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -1,5 +1,6 @@
 //Alex Stalter
-//also Jack Nichols
+//Jack Nichols
+//Joe Silveira
 //#include <stdio.h>
 
 void printString(char*);
@@ -8,27 +9,18 @@ void readString(char*);
 void readSector(char*,int);
 void handleInterrupt21(int,int,int,int,int);
 void readFile(char*,char*,int*);
+void writeSector(char*, int);
 void executeProgram(char*);
 void terminate();
+void listDir();
+void deleteFile(char*);
+
+
 void main()
 {
-	//char buffer[13312];
-	//int sectorsRead;
+
 	makeInterrupt21();
-	interrupt(0x21,5,"tstpr2",0,0);
-	//interrupt(0x21,4,"tstpr1",0,0);
-	//makeInterrupt21();
-	//interrupt(0x21,3,"messag",buffer,&sectorsRead);
-	//if(sectorsRead>0){
-
-	//	interrupt(0x21,0,buffer,0,0);
-
-	//}else{
-
-	//interrupt(0x21,0,"messag not found\r\n",0,0);
-
-	//}
-
+	interrupt(0x21,4,"shell",0,0);
 	while(1);
 
 }
@@ -88,6 +80,12 @@ void readSector(char* buffer,int sector){
 
 
 }
+void writeSector(char* buffer, int sector){
+	int ax = 3*256+1;
+	int cx = 0*256+(sector + 1);
+	int dx = 0*256+0x80;
+	interrupt(0x13,ax,buffer,cx,dx);
+}
 void handleInterrupt21(int ax,int bx,int cx,int dx){
 
 	if(ax==0){
@@ -112,6 +110,22 @@ void handleInterrupt21(int ax,int bx,int cx,int dx){
 	
 	terminate();
 
+	}else if(ax==6){
+
+	writeSector(bx,cx);
+
+	}else if(ax==7) {
+
+	deleteFile(bx);
+
+	}else if(ax==8) {
+
+
+
+	}else if(ax==9){
+
+	listDir();
+
 	}else{
 
 	printString("ERROR no such function exists\n");
@@ -120,8 +134,8 @@ void handleInterrupt21(int ax,int bx,int cx,int dx){
 
 
 }
-
-void readFile(char* address,char* buffer,int* sectorsRead){
+//old readFile
+/*void readFile(char* address,char* buffer,int* sectorsRead){
 	int count=0;
 	int i=0;
 	int matches = 0;
@@ -144,7 +158,7 @@ void readFile(char* address,char* buffer,int* sectorsRead){
 
 		}else{
 
-		matches==0;
+		matches=0;
 		break;
 		}
 
@@ -164,13 +178,157 @@ void readFile(char* address,char* buffer,int* sectorsRead){
 }
 
 }
+*/
+//New readFile written by Joe Silveira
+void readFile(char* fileName , char* fileBuffer,int* sectorsRead) {
+
+//Variables
+	char fileDirectory[512];
+	char buffer[512];
+
+	//for loop variables 
+
+	int i = 0;	//Directory iteration
+	int k = 0;	//File name iteration
+	int j = 0;	//sector iteration
+	int l = 0;	//Inner sector iteration (# sectors)
+
+	int matches = 0;	
+
+	/*
+		*Function begin
+	*/
+
+	//1. Load file in to 512 char array
+	readSector(buffer,2);
+
+	//2. Loop through the directory 
+	for(i = 0; i < 512; i++){
+
+		//2.1 try to match the file name
+		if(buffer[i] != fileName[matches]){
+
+			//2.2 if it doesnt match go to the next sector
+			i+=31;
+			matches = 0;
+			*sectorsRead = *sectorsRead +1;
+		}else if (matches == 5){
+			//printString("Matches \n");
+			
+
+			//Loop through the remaining 26 bytes (sectors)
+			for(j = 0; j < 26; j++){
+
+				//If the next sector is empty (== '\0') set the filebuffer to \0 and break
+				if(buffer[j + i + 1] == '\0'){
+
+					fileBuffer[j * 512] = '\0';
+
+					//printString("setting to 0\n");
+					break;
+
+				//If not read the sector 
+				}else{
+
+				//printString("Reading Sectors\n");
+
+				//Read the sectors and set them equal to the given sector
+					readSector(fileDirectory,buffer[j + i + 1]);
+
+				//3C: This is where we eliminate sectors read and have the for loop perform 
+					for(l = 0; l < 512; l++){
+
+					//Set the passed sector equal to the directory sector
+						fileBuffer[(j * 512) + l] = fileDirectory[l];
+					}
+				}
+			}
+			break;
+
+		//buffer[i] does equal fileBuffer[macthes]	so ++;
+		}else{
+			matches++;
+		}
+
+	}
+	if(matches==0)
+	{
+
+	*sectorsRead=0;
+
+	}
+	//return;
+}
+
+
+
+
+
+
+void listDir(){
+
+
+
+	//local vars
+	char directoryBuffer[512];
+	char buffer[512];
+	int i;
+	int j;
+	int k = 0;
+	int l = 0;
+	int size = 0;
+
+
+	//Set both buffers at i to 0 to start
+	for(i = 0; i < 512; i++){
+		directoryBuffer[i] = 0x0;
+		buffer[i] = 0x0;
+	}
+
+	//Read the given sector of the directory
+	readSector(directoryBuffer,2);
+
+	//loop through the sector 
+	for(i = 0; i < 16; i++){
+
+		//if file exists with in the given sector
+		if(directoryBuffer[32 * i] != '\0'){
+
+			//Read the file name 
+			for(j = 0; j < 6; j++){
+
+				buffer[k] = directoryBuffer[i * 32 + j];
+				k++;		
+				
+
+			}
+
+			//Add a new line and return for every file 
+			//print nicely on its own line
+			buffer[k] = '\r';
+			k++;
+			buffer[k] = '\n';
+			k++;
+			
+		}
+	}
+	printString("\n\r");
+	for(i = 0; i < 512; i++){
+
+		if(buffer[i] != 0x0){
+			printChar(buffer[i]);
+		}	
+	}
+
+}
 void executeProgram(char* name)
 {
 	int i;
+	int sectorsRead;
 	char buffer[13312];
 	int segment = 0x2000;
 
-	readFile(name, buffer);
+	readFile(name, buffer, &sectorsRead);
 
 	for(i = 0; i < 13312; i++)
 	{
@@ -184,7 +342,122 @@ void executeProgram(char* name)
 
 void terminate()
 {
-	while(1);
+
+	char shell[6];
+	shell[0] = 's';
+	shell[1] = 'h';
+	shell[2] = 'e';
+	shell[3] = 'l';
+	shell[4] = 'l';
+	shell[5] = '\0';
+	executeProgram(shell);
+
+}
+void deleteFile(char* name)
+{
+	char dir[512];
+	char map[512];
+	int matches;
+	int i, j;
+	int found, index;
+
+	readSector(map, 1);
+	readSector(dir, 2);
+	for(i = 0;i<512;i+=32){
+
+	for(j=0;j<6;j++){
+
+	if(name[j]!=dir[j+i]){
+
+	break;
+
+	}else if(j==5){
+
+	matches=1;
+	break;
+
+	}
+
+	}
+
+	if(matches==1){
+
+	dir[i]=0x0;
+	for(found=6;found<32;found++){
+
+	if(dir[i+found]!=0){
+
+	matches=dir[i+found];
+	map[matches]=0;
+
+	}else{
+
+	break;
+
+	}
+
+	}
+	writeSector(dir,2);
+	writeSector(map,1);
+	break;
+	}
+
+	}
+
 }
 
+void writeFile(char* name,char* buffer, int numberOfSectors)
+{
+	char map[512];
+	char directory[512];
+	int i, j, k;
+	int secNum, found;
 
+
+	readSector(map,1);
+	readSector(directory,2);
+
+
+        for (i = 0; i < 16; i++)
+	{
+
+		if (directory[32*i] == 0x00)
+		{
+			found = 1;
+			break;
+		}
+	}
+	if (found == 0)
+	{
+		printString("No empty space.");
+		return;
+	}
+
+	for(j = 0 ; name[j] != 0x00 ; j++)
+	{
+		directory[i*32 +j] = name[j];
+	}
+
+	for (j; j < 6; j++)
+	{
+	directory[i*32+j] = 0x00;
+	}
+
+
+	for (k = 0; k < numberOfSectors; k++)		
+	{
+
+		secNum = 0;
+		while(map[secNum] != 0x0)
+		{
+		secNum++;
+		}
+		if (secNum==26)
+		{
+			printString("Not enough space in directory\n");
+			return;
+		}
+
+		map[secNum] = 0xFF;
+	}
+}
